55dc12af4c490424ac0130ad5cf060b70b9e72f0
leticiaSouzaDev <lec667121@gmail.com>
2024-10-22 18:07:14 -0300

    feat: adicionando e testando a estrutura de rotas do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: '/li',
+        element: <h1>oiiii</h1>
+      },
+      {
+        path: '/lele',
+        element: <p>lelele</p>
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
